108eeb621a3d00d04a94f30a492520d8895e50c4
Vahid Taghizadeh <vah7id@gmail.com>
2017-05-20 14:52:38 +0430

    download as json and csv feature

--- src/components/ModelList.vue
+++ src/components/ModelList.vue
@@ -32,8 +32,8 @@
 
         <mu-popover :trigger="trigger" :open="open" @close="handleClose">
           <mu-menu>
-            <mu-menu-item title="JSON OBJECT" />
-            <mu-menu-item title="CSV FORMAT" />
+            <mu-menu-item v-on:click="download_as_json()" title="JSON OBJECT" />
+            <mu-menu-item v-on:click="download_as_csv()" title="CSV FORMAT" />
           </mu-menu>
         </mu-popover>
 
@@ -342,6 +342,76 @@
           },1000);
 
         },
+
+        download_as_json(){
+
+          var uri = 'data:text/csv;charset=utf-8,' + escape(JSON.stringify(this.items));
+          var link = document.createElement("a");    
+          link.href = uri;
+          link.style = "visibility:hidden";
+          link.download = 'Data_Report'+ ".json";
+          
+          document.body.appendChild(link);
+          link.click();
+          document.body.removeChild(link);
+
+        },
+
+        download_as_csv(){
+
+          var data = this.items;
+          if(data == '')
+              return;
+          
+          this.JSON2CSV(data, "Data_Report", true);
+
+        },
+
+        JSON2CSV(JSONData, ReportTitle, ShowLabel) {
+
+          var arrData = typeof JSONData != 'object' ? JSON.parse(JSONData) : JSONData;
+          
+          var CSV = '';    
+          CSV += ReportTitle + '\r\n\n';
+
+          if (ShowLabel) {
+              var row = "";
+              
+              for (var index in arrData[0])
+                  row += index + ',';
+
+              row = row.slice(0, -1);
+              CSV += row + '\r\n';
+          }
+          
+          for (var i = 0; i < arrData.length; i++) {
+              var row = "";
+
+              for (var index in arrData[i])
+                  row += '"' + arrData[i][index] + '",';
+         
+              row.slice(0, row.length - 1);
+              CSV += row + '\r\n';
+          }
+
+          if (CSV == '') {        
+              alert("Invalid data");
+              return;
+          }   
+          
+          var fileName = "MyReport_";
+          fileName += ReportTitle.replace(/ /g,"_");   
+          var uri = 'data:text/csv;charset=utf-8,' + escape(CSV);
+          var link = document.createElement("a");    
+          link.href = uri;
+          link.style = "visibility:hidden";
+          link.download = fileName + ".csv";
+          
+          document.body.appendChild(link);
+          link.click();
+          document.body.removeChild(link);
+      },
+
         doSearch(){
           var input, filter, table, tr, td, i;
           input = this.search;
