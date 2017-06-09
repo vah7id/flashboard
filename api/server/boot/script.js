15dff11089a87f496e3048ad342e648170ffbeb6
Vahid Taghizadeh <vah7id@gmail.com>
2017-06-09 17:15:59 +0430

    add docs

--- api/server/boot/script.js
+++ api/server/boot/script.js
@@ -5,25 +5,28 @@ module.exports = function (app) {
   var RoleMapping = app.models.RoleMapping;
   var ACL = app.models.ACL;
 
-User.create([
-    {username: 'flashboard', email: 'admin@flashboard.com', password: 'qwertyuiop', type: 'admin'}
-], function(err, users) {
+User.create([{
+      username: 'flashboard',
+      email: 'admin@flashboard.com',
+      password: 'qwertyuiop', 
+      type: 'admin'
+   }], function(err, users) {
 
   Role.find({ name: 'admin' }, function(err, results) {
-    if (err) console.log(err);
+      //if (err) console.log(err);
 
       // Create the admin role
       Role.create({
         name: 'admin'
       }, function(err, role) {
-        if (err) console.log(err);
+        if (err);
         //debug(role);
         role.principals.create({
           principalType: RoleMapping.USER,
           principalId: users[0].id
         }, function(err, principal) {
           if (err) console.log(err);
-         console.log(principal);
+        
         });
       });
 
--- api/server/server.js
+++ api/server/server.js
@@ -10,7 +10,13 @@ app.start = function() {
   return app.listen(function() {
     app.emit('started');
     var baseUrl = app.get('url').replace(/\/$/, '');
+
+    console.log('\x1b[32m', '\n******************************************* \n')
+    console.log('\x1b[32m', 'FLASHBOARD Services started successfully :) \n');
+    console.log('\x1b[32m', '******************************************* \n');
+
     console.log('Web server listening at: %s', baseUrl);
+
     if (app.get('loopback-component-explorer')) {
       var explorerPath = app.get('loopback-component-explorer').mountPath;
       console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
